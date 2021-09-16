9d94f28126efbb5f4209ba6409fcdabcf35de68e
Anshul Malviya <88474736+anshulmalviya1@users.noreply.github.com>
2021-09-16 19:32:40 +0530

    Add files via upload

---
+++ Tasks/Task5.py
@@ -0,0 +1,12 @@
+#To do list
+       elif 'remember that' in query:
+            speak("what should i remember sir")
+            rememberMessage = takeCommand()
+            speak("you said me to remember"+rememberMessage)
+            remember = open('data.txt', 'w')
+            remember.write(rememberMessage)
+            remember.close()
+
+        elif 'do you remember anything' in query:
+            remember = open('data.txt', 'r')
+            speak("you said me to remember that" + remember.read())
